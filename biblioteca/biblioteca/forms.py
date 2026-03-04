1444458f3b59a37d44fd13c73e4a29ca184a495d
kinhasacramento@gmail.com <kinhasacramento@gmail.com>
2026-03-04 16:09:30 -0300

    atualizando forms

--- biblioteca/biblioteca/forms.py
+++ biblioteca/biblioteca/forms.py
@@ -1,15 +1,28 @@
 #criando um formulário para atualizar os dados do livro
 from django import forms
-from .models import Livro
+from ProjetoFinal_Cepedi.biblioteca.gestao.models import Cliente, Livro, Emprestimo, ItemEmprestimo, Movimentacao
 
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['titulo', 'autores', 'quantidade_total', 'categoria', 'ano_lancamento']
-        widgets = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'autores': forms.TextInput(attrs={'class': 'form-control'}),
-            'quantidade_total': forms.NumberInput(attrs={'class': 'form-control'}), 
-            'categoria': forms.TextInput(attrs={'class': 'form-control'}),
-            'ano_lancamento': forms.NumberInput(attrs={'class': 'form-control'}),
-        }           
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'cpf']
+
+class EmprestimoForm(forms.ModelForm):
+    class Meta:
+        model = Emprestimo
+        fields = ['livro', 'cliente', 'data_prevista_devolucao']
+
+class ItemEmprestimoForm(forms.ModelForm):
+    class Meta:
+        model = ItemEmprestimo
+        fields = ['emprestimo', 'livro', 'quantidade']
+
+class MovimentacaoForm(forms.ModelForm):
+    class Meta:
+        model = Movimentacao
+        fields = ['livro', 'tipo', 'quantidade', 'usuario'] 
