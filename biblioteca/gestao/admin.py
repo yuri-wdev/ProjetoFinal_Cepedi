ce27f1165f78eb4460afd2e25e6eb1b57b50b552
kinhasacramento@gmail.com <kinhasacramento@gmail.com>
2026-03-04 13:45:46 -0300

    admin

--- biblioteca/gestao/admin.py
+++ biblioteca/gestao/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Livro, Emprestimo, ItemEmprestimo, Movimentacao
 
-# Register your models here.
+#administração dos modelos para o painel de controle do django
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'quantidade')
+    search_fields = ('titulo', 'autor')
+
+#admin.register(Emprestimo)
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = ('livro', 'tipo', 'quantidade', 'data')
+    list_filter = ('tipo', 'data')
+
+#admin.register(ItemEmprestimo)
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'data_emprestimo')
+    list_filter = ('data_emprestimo',)
+
+#admin.register(ItemEmprestimo)
+@admin.register(ItemEmprestimo)
+class ItemEmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('emprestimo', 'livro', 'quantidade')
+    list_filter = ('emprestimo', 'livro')
